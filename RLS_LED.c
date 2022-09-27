e73221b5564b73f760b5ad94f08482994d81a2a4
Chrid <christopher.s.andrew@gmail.com>
2022-09-27 12:59:52 -0500

    Longer LED PWM period, Smoother Transitions

--- RLS_LED.c
+++ RLS_LED.c
@@ -9,8 +9,9 @@
 //####################################################################################################################################################################
 //#Define
 //####################################################################################################################################################################
-#define PWM_PULSE_LENGTH 1000
-
+#define PWM_PULSE_LENGTH 					20000 //currently max value is roughtly 214,748 or 372 Hz PWM @ 80 MHz MCU clock
+#define DEFAULT_BRIGHTNESS_CHANGE_PERCENT	5
+#define DEFAULT_TINT_CHANGE_PERCENT			5
 //PF2 Blue
 //PF1 Red
 //####################################################################################################################################################################
@@ -19,7 +20,7 @@
 /* Standard libs */
 #include <stdint.h>
 #include <stdbool.h>
-#include <stdio.h>
+//#include <stdio.h>
 
 /* Constants */
 #include <inc/hw_ints.h>
@@ -28,43 +29,34 @@
 #include <inc/hw_gpio.h>
 #include <inc/hw_pwm.h>
 
-/* XDC */
-#include <xdc/std.h>
-#include <xdc/runtime/System.h>
-#include <xdc/runtime/Log.h>                //needed for any Log_info() call
-#include <xdc/cfg/global.h>
 
 /* DriverLib */
 #include <driverlib/sysctl.h>
 #include <driverlib/pin_map.h>
 #include <driverlib/gpio.h>
-#include <driverlib/uart.h>
-#include "driverlib/interrupt.h"
-#include "driverlib/adc.h"
-#include "driverlib/timer.h"
 #include "driverlib/pwm.h"
 
 /* TI-RTOS BIOS  */
-#include <ti/sysbios/BIOS.h>
-#include <ti/sysbios/knl/Task.h>
+//#include <ti/sysbios/BIOS.h>
+//#include <ti/sysbios/knl/Task.h>
 
-/* TI-RTOS Peripherals */
-#include <ti/drivers/GPIO.h>
-#include <ti/drivers/UART.h>
 //####################################################################################################################################################################
-// define needed varribles
+// define needed local varribles
 //####################################################################################################################################################################
 
 
 struct state {
-	unsigned int blueLightPercent;
-	unsigned int redLightPercent;
-	unsigned int brightness;
+	int blueLightPercent;
+	int redLightPercent;
+	int tintChange;
+	int brightness;
+	int brightnessChange;
 	int LedSetupComplete;
 	int Led_On;
+	int mode; //mode 0 basic brightten darken cycles
 };
 
-struct state LED_State;
+static struct state LED_State;
 
 
 int RLS_LED_Setup()
@@ -73,6 +65,8 @@ int RLS_LED_Setup()
 	LED_State.blueLightPercent = 0;
 	LED_State.redLightPercent = 100;
 	LED_State.brightness = 100;
+	LED_State.brightnessChange = DEFAULT_BRIGHTNESS_CHANGE_PERCENT;
+	LED_State.tintChange = DEFAULT_TINT_CHANGE_PERCENT;
 
 	//configure GPIO pins
 	GPIOPinTypePWM(GPIO_PORTF_BASE, GPIO_PIN_1);	// Configures PB6 for typical PWM settings
@@ -81,7 +75,7 @@ int RLS_LED_Setup()
 	GPIOPinConfigure(GPIO_PF2_M1PWM6);
 
 	//configure PWM basline
-	//enable PWM module 0 and wait for it to come online
+	//enable PWM module and wait for the module to be ready for configuration
 	SysCtlPeripheralEnable(SYSCTL_PERIPH_PWM1);
 	while(!SysCtlPeripheralReady(SYSCTL_PERIPH_PWM1)){}
 
@@ -107,14 +101,15 @@ int RLS_LED_Setup()
 	//BUTTON SETUP FOR QUICK TESTING
 	//MOVE TO ANOTHER FILE LATTER MAYBE? ALONG WITH DEBOUNCING?
 
-	//magical unlocking thing?
+	//magical unlocking thing? may not be needed?
 	HWREG(GPIO_PORTF_BASE + GPIO_O_LOCK) = GPIO_LOCK_KEY;
 	HWREG(GPIO_PORTF_BASE + GPIO_O_CR) |= 0x01;
 	HWREG(GPIO_PORTF_BASE + GPIO_O_LOCK) = 0;
 
-	GPIOPadConfigSet(GPIO_PORTF_BASE, GPIO_PIN_0 | GPIO_PIN_4, GPIO_STRENGTH_2MA, GPIO_PIN_TYPE_STD_WPU);
+	GPIOPadConfigSet(GPIO_PORTF_BASE, GPIO_PIN_0 | GPIO_PIN_4, GPIO_STRENGTH_8MA, GPIO_PIN_TYPE_STD_WPU);
 	GPIOPinTypeGPIOInput(GPIO_PORTF_BASE, GPIO_PIN_0 | GPIO_PIN_4);
 
+	//Clock task currently setup in RTOS Config, look into setting up here instead latter
 	return 0;
 }
 
@@ -122,42 +117,55 @@ void RLS_LED_Update_Task()
 {
 	if(LED_State.LedSetupComplete == 1)
 	{
-		//TODO MAKE Brightness/color cycling clearner and have full cycles, maybe seperate out into functions
+		//TODO MAKE Brightness/color cycling clearner and have full cycles, maybe seperate out into functions?
 		//LED Button Checks
-		int OTHERTEST = GPIOPinRead(GPIO_PORTF_BASE, GPIO_PIN_0);
+
 		if(GPIOPinRead(GPIO_PORTF_BASE, GPIO_PIN_0) == 0)
 		{
 			//increment brightness slowly then reset to min at max brightness
-			LED_State.brightness = LED_State.brightness + 5;
-			if(LED_State.brightness  > 100)
+			LED_State.brightness = LED_State.brightness + LED_State.brightnessChange;
+			if(LED_State.brightness > 100) //check for brightness going over 100%
+			{
+				LED_State.brightness = 100;
+				LED_State.brightnessChange = -1 * LED_State.brightnessChange;
+			}
+			else if((LED_State.brightness - LED_State.brightnessChange) < 0) //check for brightness going under 0%
 			{
 				LED_State.brightness = 0;
+				LED_State.brightnessChange = -1 * LED_State.brightnessChange;
 			}
 		}
 
-		int TESTPLSREMOVE = GPIOPinRead(GPIO_PORTF_BASE, GPIO_PIN_4);
 		//adjust red/blue light
 		if(GPIOPinRead(GPIO_PORTF_BASE, GPIO_PIN_4) == 0)
 		{
-			LED_State.redLightPercent = LED_State.redLightPercent - 5;
-			LED_State.blueLightPercent = LED_State.blueLightPercent + 5;
-
-			if(LED_State.redLightPercent == 0 || LED_State.blueLightPercent == 100)
+			//update light percent
+			LED_State.redLightPercent = LED_State.redLightPercent - LED_State.tintChange;
+			LED_State.blueLightPercent = LED_State.blueLightPercent + LED_State.tintChange;
+			//then check boundries
+			if(LED_State.redLightPercent > 100 || LED_State.blueLightPercent < 0) //check if red goes over 100 and flips tinting and corrects boundries
 			{
 				LED_State.redLightPercent = 100;
 				LED_State.blueLightPercent = 0;
+				LED_State.tintChange = -1 * LED_State.tintChange;
 			}
+			else if(LED_State.blueLightPercent > 100 || LED_State.redLightPercent < 0) //checks if blue goes over 100% and flips tinting and corrects boundries
+			{
+				LED_State.redLightPercent = 0;
+				LED_State.blueLightPercent = 100;
+				LED_State.tintChange = -1 * LED_State.tintChange;
+			}
+
 		}
 
 		//LED Update Cycle
 		if(LED_State.Led_On == 1)
 		{
-			PWMOutputState(PWM1_BASE, PWM_OUT_5_BIT, true);	// enables the outputs
-			PWMOutputState(PWM1_BASE, PWM_OUT_6_BIT, true);
 
 			//PWM Pulse update
-			unsigned int blue = (PWM_PULSE_LENGTH * LED_State.blueLightPercent * LED_State.brightness)/(100*100);
-			unsigned int red  = (PWM_PULSE_LENGTH * LED_State.redLightPercent * LED_State.brightness)/(100*100);
+			int blue = (PWM_PULSE_LENGTH * LED_State.blueLightPercent * LED_State.brightness)/(100*100);
+
+			int red  = (PWM_PULSE_LENGTH * LED_State.redLightPercent * LED_State.brightness)/(100*100);
 
 			if(red > 0)
 				PWMPulseWidthSet(PWM1_BASE, PWM_OUT_5, red);
--- RLS_LED.h
+++ RLS_LED.h
@@ -14,7 +14,9 @@
 void RLS_LED_Update_Task();
 
 int RLS_LED_Setup();
-
+int RLS_LED_Color_Update(unsigned int redLight, unsigned int blueLight);
+int RLS_Brightness_Update(unsigned int brightness);
+int RLS_LED_Control(unsigned int onOff);
 
 
 #endif /* RLS_LED_H_ */
